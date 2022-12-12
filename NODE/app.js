826379f4f220658ad1e1209b9fc1319469fcb5e9
dilip765 <87632038+dilip765@users.noreply.github.com>
2022-12-12 13:10:36 +0530

    second commit

--- NODE/app.js
+++ NODE/app.js
@@ -89,43 +89,6 @@ app.listen(5000, () => {
   console.log("Server Started");
 });
 
-app.post("/forgot-password", async (req, res) => {
-  const { email } = req.body;
-  try {
-    const oldUser = await User.findOne({ email });
-    if (!oldUser) {
-      return res.json({ status: "User Not Exists!!" });
-    }
-    const secret = JWT_SECRET + oldUser.password;
-    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
-      expiresIn: "5m",
-    });
-    const link = `http://localhost:5000/reset-password/${oldUser._id}/${token}`;
-    var transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "adarsh438tcsckandivali@gmail.com",
-        pass: "rmdklolcsmswvyfw",
-      },
-    });
-
-    var mailOptions = {
-      from: "youremail@gmail.com",
-      to: "thedebugarena@gmail.com",
-      subject: "Password Reset",
-      text: link,
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
-    });
-    console.log(link);
-  } catch (error) {}
-});
 
 app.get("/reset-password/:id/:token", async (req, res) => {
   const { id, token } = req.params;
